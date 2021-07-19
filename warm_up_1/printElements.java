2cafab5ecf917c964b9be8614d71d7b4aed38ff7
Savitha <savitha.r@caratlane.com>
2021-07-19 21:47:51 +0530

    print Array elements

--- warm_up_1/printElements.java
+++ warm_up_1/printElements.java
@@ -1,6 +1,26 @@
 import java.util.*;
+import java.io.*;
 
+public class printElements{
 
+public static boolean printele(int[] arr,int size){
+    if (size <= 0) return false;
+    for(int ele : arr){
+        System.out.println(ele);
+    }
+    return true;
+}
 public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+      int size = sc.nextInt();
+      int[] arr=new int[size];
+      for(int i=0;i<size;i++){
+        arr[i] = sc.nextInt();
+      }
+      boolean res = printele(arr,size);
+      
+}
+
+
 
 }
